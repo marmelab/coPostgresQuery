e92e32372a363c07add7d10be4845604b3f80229
ThieryMichel <thiery@marmelab.com>
2015-09-30 17:18:00 +0200

    simplify valueSubQuery

--- test/base/valueSubQuery.js
+++ test/base/valueSubQuery.js
@@ -7,7 +7,7 @@ describe('base/valueSubQuery', function () {
     describe('getValueSubQuery', function () {
 
         it('should return subQuery filtering out unwanted field', function () {
-            var valueSubQueryGenerator = valueSubQuery(['id', 'login', 'first_name'], true, 'id');
+            var valueSubQueryGenerator = valueSubQuery(['login', 'first_name']);
             assert.deepEqual(valueSubQueryGenerator({ id: 5, login: 'john', first_name: 'doe', password: '$ecret' }), {
                 query: '$login, $first_name',
                 parameters: {
@@ -19,7 +19,7 @@ describe('base/valueSubQuery', function () {
         });
 
         it('should return subQuery with given suffix', function () {
-            var valueSubQueryGenerator = valueSubQuery(['id', 'login', 'first_name'], true, 'id');
+            var valueSubQueryGenerator = valueSubQuery(['login', 'first_name']);
             assert.deepEqual(valueSubQueryGenerator({ id: 5, login: 'john', first_name: 'doe', password: '$ecret' }, 2), {
                 query: '$login2, $first_name2',
                 parameters: {
@@ -27,32 +27,6 @@ describe('base/valueSubQuery', function () {
                     first_name2: 'doe'
                 },
                 columns: ['login', 'first_name']
-            });
-        });
-
-        it('should return subQuery with all passed field if whiteListed is set to true', function () {
-            var valueSubQueryGenerator = valueSubQuery(['id', 'login', 'first_name'], true, 'id');
-            assert.deepEqual(valueSubQueryGenerator({ id: 5, login: 'john', first_name: 'doe', password: '$ecret' }, null, true), {
-                query: '$login, $first_name, $password',
-                parameters: {
-                    login: 'john',
-                    first_name: 'doe',
-                    password: '$ecret'
-                },
-                columns: ['login', 'first_name', 'password']
-            });
-        });
-
-        it('should return subQuery with id field if raw is true', function () {
-            var valueSubQueryGenerator = valueSubQuery(['id', 'login', 'first_name'], true, 'id');
-            assert.deepEqual(valueSubQueryGenerator({ id: 5, login: 'john', first_name: 'doe', password: '$ecret' }, null, false, true), {
-                query: '$id, $login, $first_name',
-                parameters: {
-                    id: 5,
-                    login: 'john',
-                    first_name: 'doe'
-                },
-                columns: ['id', 'login', 'first_name']
             });
         });
     });
