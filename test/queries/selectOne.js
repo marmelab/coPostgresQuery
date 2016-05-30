42bd623aa470956994efa2c5d656d1d58c6e7b84
ThieryMichel <thiery@marmelab.com>
2016-05-30 17:19:13 +0200

    accept single value for selectOne parameter

--- test/queries/selectOne.js
+++ test/queries/selectOne.js
@@ -13,6 +13,16 @@ describe('QUERY selectOne', function () {
         });
     });
 
+    it('should generate sql and parameter for selecting one entity when receiving single value as parameter', function () {
+        const selectOneQuery = selectOneQuerier('bib_user', [ 'username' ], ['*']);
+        assert.deepEqual(selectOneQuery('john'), {
+            sql: 'SELECT * FROM bib_user WHERE username = $username LIMIT 1',
+            parameters: {
+                username: 'john'
+            }
+        });
+    });
+
     it('should ignore parameters not in idFields', function () {
         const selectOneQuery = selectOneQuerier('table', [ 'id1', 'id2' ], ['fielda', 'fieldb']);
         assert.deepEqual(selectOneQuery({ id1: 1, id2: 2, fielda: 'a', fieldb: 'b' }), {
