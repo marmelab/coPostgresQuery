1acb8e5782c80bafa31226374a23f49dc177dd9d
ThieryMichel <thiery@marmelab.com>
2016-05-31 12:26:06 +0200

    fix upsertOne not matching field and value order on insert part

--- test/queries/upsertOne.js
+++ test/queries/upsertOne.js
@@ -21,21 +21,19 @@ RETURNING *`
         });
     });
 
-    it('should generate sql and parameter for upserting one entity ignoring autoincrement fields', function () {
-        const upsertOneQuery = upsertOneQuerier('table', [ 'id1', 'id2' ], ['fielda', 'fieldb'], ['id2']);
-        assert.deepEqual(upsertOneQuery({ id1: 1, id2: 2, fielda: 'a', fieldb: 'b' }), {
+    it('should generate sql and parameter for upserting one entity using same order for (field...) and VALUES(field...)', function () {
+        const upsertOneQuery = upsertOneQuerier('table', [ 'id' ], ['field']);
+        assert.deepEqual(upsertOneQuery({ field: 'value', id: 1 }), {
             sql: (
-`INSERT INTO table (id1, fielda, fieldb)
-VALUES ($id1, $fielda, $fieldb)
-ON CONFLICT (id1, id2) DO UPDATE
-SET fielda = $fielda, fieldb = $fieldb
+`INSERT INTO table (id, field)
+VALUES ($id, $field)
+ON CONFLICT (id) DO UPDATE
+SET field = $field
 RETURNING *`
             ),
             parameters: {
-                id1: 1,
-                id2: 2,
-                fielda: 'a',
-                fieldb: 'b'
+                id: 1,
+                field: 'value'
             }
         });
     });
